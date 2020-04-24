ae3d999c8ceae5d0f06ab330403bcdef3229a161
philip <philip@randomsmiths.com>
2020-04-23 21:58:51 -0400

    began working on latex maker

--- latex.py
+++ latex.py
@@ -1,25 +1,45 @@
-#built in functions
-import re
+import datetime
+from entries import Entry
 
-# Code from: https://stackoverflow.com/questions/16259923/how-can-i-escape-latex-special-characters-inside-django-templates
-def tex_escape(text):
-    """
-        :param text: a plain text message
-        :return: the message escaped to appear correctly in LaTeX
-    """
-    conv = {
-        '&': r'\&',
-        '%': r'\%',
-        '$': r'\$',
-        '#': r'\#',
-        '_': r'\_',
-        '{': r'\{',
-        '}': r'\}',
-        '~': r'\textasciitilde{}',
-        '^': r'\^{}',
-        '\\': r'\textbackslash{}',
-        '<': r'\textless{}',
-        '>': r'\textgreater{}',
+
+class LaTeX():
+    def getMonthName(self, monthNumber):
+
+        return datetime.date(2020, int(monthNumber), 1).strftime('%B')
+
+
+
+
+    def entriesLaTeXFromTasksDict(self, taskDict, date):
+        monthName = self.getMonthName(date[5:-3])
+        wholedate = monthName + " " + date[-2:]
+        latexString = "\\begin{center} \n"
+        latexString += "   \\subsubsection{date} \n".format(date="{"+wholedate+"}")
+        latexString += "\\begin{tabularx}{\\textwidth}{|l|X|X|} \n"
+        latexString += "\\hline Task & Details & Pictures \\\\ \\hline \n"
+
+
+        for task, entries in taskDict.items():
+            latexString += task + " & \\begin{itemize}\n"
+            for entry in entries:
+                latexString += "\\item Accomplished \n \\begin{itemize} \n  \\item " + entry.memberName + ": " + entry.accomplished + "\n \\end{itemize}"
+
+
+
+            latexString += "\\\\ \hline \n"
+
+        print(latexString)
+
+
+        
+
+
+
+if __name__ == "__main__":
+    latex = LaTeX()
+    testDict = {
+        "task1":[Entry("2019-12-07", "task1", "Philip Smith", "Accomplished", "why", "Learned", "nextSteps", "photoLink", "imgKey")],
+        "task2":[Entry("2019-12-07", "task2", "Philip Smith", "Accomplished", "why", "Learned", "nextSteps", "photoLink", "imgKey")]
     }
-    regex = re.compile('|'.join(re.escape(str(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
-    return regex.sub(lambda match: conv[match.group()], text)
+    latex.entriesLaTeXFromTasksDict(testDict, "2019-12-07")
+    print("nothing else for me to do")
