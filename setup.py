c84b6fa5f51b0df98f3d95cb71fe1051db5212b8
philip <philip@randomsmiths.com>
2020-05-16 16:11:39 -0400

    added a setup file to begin the db

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+
+from entries import Entries
+from members import Members
+from tasks import Tasks
+
+entries = Entries()
+members = Members()
+tasks = Tasks()
+
+memberList = ["Andrew", "Chirag", "Eric", "Izaak", "Nithya", "Philip", "Preeti", "Rishi", "Arjun", "Ryan", "Nikhil"]
+
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'data/database.sqlite3')
+
+try:
+    os.remove(DEFAULT_PATH)
+except IOError:
+    pass #delete File, if it doesn't exist we don't care
+
+with sqlite3.connect(DEFAULT_PATH) as connection:
+    entries.createTable(connection)
+    members.createTable(connection)
+    members.insertMembers(connection, memberList)
+    tasks.createTable(connection)
+    
+
+
